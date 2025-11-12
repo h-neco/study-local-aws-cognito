a397b19115318f33147d4c3fa03fcf21b97173fd
hneko <>
2025-11-12 23:08:17 +0900

    feat: 🎸 dynamodb fix

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,10 +1,10 @@
-resource "aws_dynamodb_table" "user_table" {
-  name         = "${var.env.name}UserTable"
+resource "aws_dynamodb_table" "this" {
+  name         = "${var.env.name}Table"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "user_id"
+  hash_key     = "log_id"
 
   attribute {
-    name = "user_id"
+    name = "log_id"
     type = "S"
   }
 
@@ -16,14 +16,3 @@ resource "aws_dynamodb_table" "user_table" {
     enabled = true
   }
 }
-
-#resource "aws_dynamodb_table" "session_table" {
-#  name         = "${var.local.env}SessionTable"
-#  billing_mode = "PAY_PER_REQUEST"
-#  hash_key     = "session_id"
-#
-#  attribute {
-#    name = "session_id"
-#    type = "S"
-#  }
-#}
